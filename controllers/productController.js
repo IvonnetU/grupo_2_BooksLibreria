eccda2a92a1bcaa9b024b0e1720fedb2ef5c732d
Katecr <katecr17@gmail.com>
2021-11-22 22:18:09 -0500

    [Add] Listado de productos desde el json

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,14 +2,19 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+ const fs = require('fs');
 
 //asignando la carpeta public como recurso estatico
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
+//conectando json
+const productsFilePath = path.join(__dirname, '../data/products.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
 let productController = {  
   list: (req, res) => {
-    res.render('./products/productList');
+    res.render('./products/productList',{ dataBooks: products });
   },
   detail: (req, res) => {
     res.render('./products/productDetail');
